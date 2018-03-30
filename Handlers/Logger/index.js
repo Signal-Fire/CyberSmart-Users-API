599b0d063b6be8f6b26b28672e89af8e4666ceb6
Henry Pye <henrypye@outlook.com>
2018-03-30 14:40:26 +0100

    Added logging functionality + class ooooh yeeee

--- Handlers/Logger/index.js
+++ Handlers/Logger/index.js
@@ -1,10 +1,28 @@
+var request = require('request'),
+    config = require('../../Configuration');
+
 module.exports = class Logger {
     constructor() {
 
     }
 
-    Create(log) {
+    Create(message, createdBy) {
+        if (!message || !createdBy)
+            return true;
 
+        request.post({url:config["logger-url"] + '/create', 
+            body: JSON.stringify({
+                message : message,
+                created_by_user : createdBy
+            }),
+            headers : {
+                'Content-Type' : 'application/json'
+            }}, 
+            function optionalCallback(err, httpResponse, body) {
+                if (err) 
+                    return false;
+                return true;
+            });
     }
 
     FindAll() {
